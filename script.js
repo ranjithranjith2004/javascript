eca5e748644bac0df1d475474df89c55b912697d
ranjithranjith2004 <mranjithips957@gmail.com>
2025-09-04 16:17:20 +0530

    add

---
+++ script.js
@@ -0,0 +1,50 @@
+//Arithmetic Operators
+let a=5;
+let b=2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//Assignment Operato
+let aa=10;
+let bb=5;
+console.log(aa+=2,bb+=2);
+console.log(aa-=2,bb-=2);
+console.log(aa*=2,bb*=2);
+console.log(aa/=2,bb/=2);
+console.log(aa%=2,bb%=2);
+
+
+//Comparison Operators
+let x=7;
+let y="5";
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+console.log(x!==y);
+console.log(x>y);
+console.log(x<y);
+console.log(x<=y);
+console.log(x>=y);
+
+//Logical Operators
+let p=true;
+let q=false;
+console.log(p && q); 
+console.log(p || q);
+console.log(!p);    
+
+//Increment & Decrement
+let num = 7;
+console.log(num++); 
+console.log(++num); 
+console.log(num--); 
+console.log(--num); 
+
+//String Operator
+let first = "Hello";
+let second = "sla";
+console.log(first + " " + second);
